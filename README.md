959512adbf07672e0bb2be420decce811b7f5abe
ginakui <developerkui@outlook.com>
2019-09-08 21:21:36 -0400

    first commit, deleted redundant .git at client folder, updated express-validator pkg

---
+++ README.md
@@ -0,0 +1,22 @@
+# Contact Book
+
+## Usage
+
+Install dependencies
+
+```bash
+npm install  
+npm client-install # Client side install
+```
+
+### Mongo connection setup
+
+Edit /config/default.json file to include the correct MongoDB URI
+
+### Run Server
+
+```bash
+npm run dev     # Express & React :3000 & :5000
+npm run server  # Express API Only :5000
+npm run client  # React Client Only :3000
+```
---
+++ routes/users.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
+
+// @route     POST api/users
+// @desc      Regiter a user
+// @access    Public
+router.post(
+  '/',
+  [
+    check('name', 'Please add a username')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    //check errors of request input
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (user) return res.status(400).json({ msg: 'User already exists' });
+      
+      //encrypt the user's password and save to database
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      user = new User({
+        name,
+        email,
+        password: hashedPassword
+      });
+      await user.save();
+
+      //send response to client with data payload and json web token
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: config.get('jwtLife')
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+module.exports = router;
