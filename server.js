db7668f620d56d54838a2255969b5b50a76e17e1
ginakui <developerkui@outlook.com>
2019-09-08 22:37:47 -0400

    installed helmet pkg to add security

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
+const helmet = require('helmet');
 
 const app = express();
 
@@ -8,6 +9,7 @@ const app = express();
 connectDB();
 
 // Init Middleware
+app.use(helmet());
 app.use(express.json({ extended: false }));
 
 // Define Routes
